249830204c0792b0af251351d668a7602806c181
Nilokrtz <nilogabriel21@gmail.com>
2023-11-24 12:16:10 -0300

    component choices

--- src/app/level-selection/level-selection.page.ts
+++ src/app/level-selection/level-selection.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-level-selection',
@@ -8,13 +7,7 @@ import { Router } from '@angular/router';
 })
 export class LevelSelectionPage implements OnInit {
 
-  constructor(private router: Router) {}
-
-  goToLevel(level: number) {
-    // Here, you can navigate to the actual game level page when it's ready
-    // For now, let's navigate to a placeholder page called 'null-page'
-    this.router.navigate(['null-page']);
-  }
+  constructor() {}
 
   ngOnInit() {
   }
