590b1ca232a0a8b2b9d85211f75474d29775fd63
Kauã Carvalho <contato.kauacarval@gmail.com>
2023-11-27 16:33:40 -0300

    Criando cenários

---
+++ src/app/middle-age-scenery/game-scene.ts
@@ -0,0 +1,45 @@
+import 'phaser';
+
+export class GameScene extends Phaser.Scene {
+  private player!: Phaser.Physics.Arcade.Sprite;
+  private nextButton!: HTMLButtonElement;
+
+  constructor() {
+    super({ key: 'game' });
+  }
+
+  preload() {
+    this.load.image('ground', 'assets/scenerys/CenarioLua.jpg');
+    this.load.spritesheet('player', 'assets/SpritesProtagonista/7.png', {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
+
+  create() {
+    // Add ground
+    const ground = this.physics.add.staticGroup();
+    ground.create(window.innerWidth / 2, window.innerHeight - 20, 'ground').setScale(2).refreshBody();
+
+    // Add player
+    this.player = this.physics.add.sprite(100, window.innerHeight - 100, 'player');
+    this.player.setCollideWorldBounds(true);
+
+    // Add button
+    this.nextButton = document.createElement('button');
+    this.nextButton.innerText = 'Next';
+    this.nextButton.style.position = 'absolute';
+    this.nextButton.style.bottom = '20px';
+    this.nextButton.style.right = '20px';
+    this.nextButton.addEventListener('click', () => this.nextButtonClick());
+    document.body.appendChild(this.nextButton);
+
+    // Set up collisions
+    this.physics.add.collider(this.player, ground);
+  }
+
+  private nextButtonClick() {
+    // Implement logic to move the player to the next NPC
+    // For example, update player coordinates and disable the button until the question is answered
+  }
+}
--- src/app/middle-age-scenery/middle-age-scenery.page.ts
+++ src/app/middle-age-scenery/middle-age-scenery.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import * as Phaser from 'phaser';
+import { GameScene } from '../middle-age-scenery/game-scene';
 
 @Component({
-  selector: 'app-middle-age-scenery',
+  selector: 'app-middle-scenery',
   templateUrl: './middle-age-scenery.page.html',
   styleUrls: ['./middle-age-scenery.page.scss'],
 })
 export class MiddleAgeSceneryPage implements OnInit {
+  private phaserGame!: Phaser.Game;
 
-  constructor() { }
+  constructor(private zone: NgZone) {}
 
   ngOnInit() {
+    this.initializeGame();
   }
 
+  private initializeGame() {
+    const config: Phaser.Types.Core.GameConfig = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 },
+        },
+      },
+      scene: [GameScene], // Use an array to include multiple scenes if needed
+    };
+
+    this.phaserGame = new Phaser.Game(config);
+  }
 }
